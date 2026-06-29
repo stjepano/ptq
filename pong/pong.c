08b6ec5f893e9470f2f8537775cdc345ede26c2f
Stjepan Obrankovic <stjepano@gmail.com>
2026-06-29 22:44:33 +0200

    Playing with tones

--- pong/pong.c
+++ pong/pong.c
@@ -41,24 +41,32 @@ static void fillSurface()
 
 typedef struct
 {
+    f32 phase;
     f32 freq;
-    f32 time;
 } sine_wave_t;
 
-sine_wave_t sine_a = {.freq = 330.0f, .time = 0.0f};
-sine_wave_t sine_b = {.freq = 150.0f, .time = 0.0f};
-sine_wave_t sine_c = {.freq = 440.0f, .time = 0.0f};
+sine_wave_t sine = {.phase = 0, .freq = 400};
+sine_wave_t tone_a = {.phase = 0, .freq = 220};
+sine_wave_t tone_b = {.phase = 0, .freq = 330};
 
-static void sineWaveSamples(sine_wave_t *sine_wave, f32 *output,
-                            u32 frame_count)
+static void sineWaveSamples(sine_wave_t *s, f32 *output, u32 frame_count)
 {
-    const f32 deltaT = 1.0f / (f64)AUDIO_SAMPLE_RATE;
+    // y = sin(2*Pi*f*t)
+    // t = total_frame_count / sample_rate
+    // y = sin(2*Pi*f/sample_rate * total_frame_count)
+    // y = sin(sum[i < total_frame_count](2*Pi*f/sample_rate))
+    // phase = sum[i < total_frame_count](2*Pi*f/sample_rate)
     for (i32 i = 0; i < frame_count; i++)
     {
-        sine_wave->time += deltaT;
-        f32 val = sin(2.0f * M_PI * sine_wave->freq * sine_wave->time);
-        *output++ += val;
-        *output++ += val;
+        f32 sample = sin(s->phase);
+        s->phase += 2.0f * M_PI * (f32)s->freq / (f32)AUDIO_SAMPLE_RATE;
+        if (s->phase > 2.0f * M_PI)
+        {
+            s->phase -= (2.0f * M_PI);
+        }
+
+        *output++ += sample;
+        *output++ += sample;
     }
 }
 
@@ -75,15 +83,16 @@ static void audioMixer(f32 *output, u32 frame_count, void *user_data)
 {
     ((void)user_data);
     MemZero(output, sizeof(f32) * frame_count * 2);
-    sineWaveSamples(&sine_b, output, frame_count);
-    sineWaveSamples(&sine_a, output, frame_count);
-    sineWaveSamples(&sine_c, output, frame_count);
+    sineWaveSamples(&sine, output, frame_count);
+    sineWaveSamples(&tone_a, output, frame_count);
+    sineWaveSamples(&tone_b, output, frame_count);
     scaleSamples(output, frame_count);
 }
 
+#define FREQ_CHANGE_PER_SECOND 400.0
 int main(int argc, char **argv)
 {
-    g_audio_scale = 0.05f;
+    g_audio_scale = 0.25f;
     platform_config_t conf = {.window_width = WINDOW_WIDTH,
                               .window_height = WINDOW_HEIGHT,
                               .window_title = WINDOW_TITLE,
@@ -99,9 +108,16 @@ int main(int argc, char **argv)
 
     f64 t0 = glfwGetTime();
     u64 frame_count = 0;
+    f64 t = t0;
+    f64 dt = 0.0;
+    ((void)dt);
     while (!glfwWindowShouldClose(g_window))
     {
         glfwPollEvents();
+
+        sine.freq =
+            400.0 + 200.0f * sin(2.0f * M_PI * 1.0f * (glfwGetTime() - t0));
+
         if (glfwGetKey(g_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         {
             glfwSetWindowShouldClose(g_window, true);
@@ -113,6 +129,9 @@ int main(int argc, char **argv)
 
         glfwSwapBuffers(g_window);
         frame_count++;
+        f64 tmpT = glfwGetTime();
+        dt = tmpT - t;
+        t = tmpT;
     }
     f64 delta_t = glfwGetTime() - t0;
     f64 fps = ((f64)frame_count) / delta_t;
